043e001e095efd725d4e4f38a3a767583f838a6d
Heliosmaster <helios89@gmail.com>
2012-01-14 16:12:57 +0100

    it works!

--- bspfft2d_transpose.c
+++ bspfft2d_transpose.c
@@ -1,60 +1,103 @@
 #include "BSPedupack/bspedupack.c"
 #include "BSPedupack/bspfft.c"
 
-
+  /**
+  * Prints the given m-by-n matrix a (mainly for debugging and code reduction)
+  * s = current processor
+  */
 void printm(double **a, int m, int n,int s){
   int i,j;
   for(i=0;i<m;i++){
         printf("%d-%d: ",s,i);
       for(j=0;j<n;j++){
-        printf("%d %d | ",(int)a[i][2*j],(int)a[i][2*j+1]);
+        printf("%f %f | ",a[i][2*j],a[i][2*j+1]);
       }
       printf("\n");
     }
 }
 
-void bspfft2d_rows(double **a,int nlr,int nlc, int sign){
+
+  /**
+  * Performs 1D FFT on every row of the nlr-by-nlc matrix a
+  * sign = 1/-1 for forward/backward fft
+  */
+void bspfft_rows(double **a,int nlr,int nlc, int sign){
   double *w;
   int *rho;
+  
+  //memory allocation for the required weight & bit reversal tables
   rho = vecalloci(nlc);
   w=  vecallocd(nlc);
+  
+  // initialization of the tables
   ufft_init(nlc,w);
   bitrev_init(nlc,rho);
-  int i;
+  
+  int i,j;
+  
+  // permutes every row and performs 1D fft according to sign
   for(i=0;i<nlr;i++){
     permute(a[i],nlc,rho);
     ufft(a[i],nlc,sign,w);
   }
-  vecfreed(w); 
+  
+  // freeing memory
+  vecfreei(rho);
+  vecfreed(w);
+  
+  // if inverse FFT 
+  if (sign==-1){
+    double ninv = 1/(double)nlc;
+    for(i=0;i<nlr;i++) for(j=0;j<nlc;j++){
+      a[i][2*j] *= ninv;
+      a[i][2*j+1] *= ninv;
+    }
+  }
 }
 
-void bspfft2d_move(double **a,int nlr,int nlc, double *pa){
+
+void transpose(double **a,int nlr,int nlc, double *pa){
   int p = bsp_nprocs();
   int s = bsp_pid();
   double *tmp;
   int i,j,destproc,destrow,destindex,iglob;
   
-  
+  //allocates a temporary vector
   tmp = vecallocd(2);
+  
   
   for(i=0;i<nlr;i++)
   for(j=0;j<nlc;j++){
-    iglob = s+i*p;
+    
+    // computes the destination processor and rows starting from the local index j
     destproc = j%p;
     destrow = j/p;
+    
+    // computes global index
+    iglob = s+i*p;
+    
+    // computes the actual index on the remote destination
     destindex = destrow*nlc+iglob;
+    
+    // stores in tmp the stuff to be sent
     tmp[0] = a[i][2*j];
     tmp[1] = a[i][2*j+1];
-//    printf(" %d: putting a%d%d=%d in %d, row %d col %d, index %d\n",s,i,j,(int)a[i][2*j],destproc,destrow,iglob,destindex);
-bsp_put(destproc,tmp,pa,destindex*2*SZDBL,2*SZDBL);
+    
+    // performs the actual comunication
+    bsp_put(destproc,tmp,pa,destindex*2*SZDBL,2*SZDBL);
   }
   bsp_sync();
 }
 
 
 void bspfft2d_transpose(double **a,int nlr,int nlc, int sign, double *pa){
-  bspfft2d_rows(a,nlr,nlc,sign);
-bspfft2d_move(a,nlr,nlc,pa);
-bspfft2d_rows(a,nlr,nlc,sign);
-bspfft2d_move(a,nlr,nlc,pa);
+  //performs fft on the rows
+  bspfft_rows(a,nlr,nlc,sign);
+  
+  //transpose and performs again fft on the rows
+  transpose(a,nlr,nlc,pa);
+  bspfft_rows(a,nlr,nlc,sign);
+  
+  //transpose it back
+  transpose(a,nlr,nlc,pa);
 }
--- bspfft2d_transpose_test.c
+++ bspfft2d_transpose_test.c
@@ -21,53 +21,59 @@ void bspfft2d_transpose_test(){
   if (s==0){
     //printf("Please enter matrix row size n0:\n");
     // scanf("%d",&n0);
-    //  if(n0<2*M) bsp_abort("Error in input: n0 < 2M");
+    // if(n0%2 != 0) bsp_abort("Please insert a multiple of 2 as n0")
     //  printf("Please enter matrix column size n1:\n");
     //  scanf("%d",&n1);
-    //  if(n1<2*N) bsp_abort("Error in input: n1 < 2N");
+    // if(n1%2 != 0) bsp_abort("Please insert a multiple of 2 as n0")
         n0=4;
         n1=4;
       for (j=0; j<p; j++){ 
         bsp_put(j,&n0,&n0,0,SZINT);
         bsp_put(j,&n1,&n1,0,SZINT);
       }
-      //if(n0<P) bsp_abort("Too small for this many procs!");
+   
   }
     
   bsp_sync();
   
-  // number of local rows (cyclic distribution w/ powers of 2)
+  // number of local rows (cyclic distribution w/ powers of 2) and columns (the length of each fft)
   nlr = n0/p;
   nlc = n1; 
    
+  // memory allocation and variable registration
   a = matallocd(nlr,2*nlc);
   bsp_push_reg(a,2*nlr*nlc*SZDBL);
   
-  //matrix creation
-  
+
+  // matrix creation
   for(i=0;i<nlr;i++)
   for(j=0;j<nlc;j++){
-    a[i][2*j]= 10*s+nlc*i+j;
-    a[i][2*j+1]= 10*s+nlc*i+j;
+    a[i][2*j]= 2.0;
+    a[i][2*j+1]= 1.0;
   }
   
+  // pointer to the beginning of the matrix, used as starting point for communication
   double *pa = a[0];
   bsp_push_reg(pa,2*nlr*nlc*SZDBL);
   
   bsp_sync();
   
- /* 
-  printf("before:\n");
-  printm(a,nlr,nlc,s);
-  */
+  // forward 2D fft
   bspfft2d_transpose(a,nlr,nlc,1,pa);
- // bspfft2d_transpose(a,nlr,nlc,-1,pa);
+  
+/*
+  sleep(1);
+  if(s==0) printf("---\n");*/
+  
+  //inverse 2d fft
+  bspfft2d_transpose(a,nlr,nlc,-1,pa);
+
+ 
+  printm(a,nlr,nlc,s);  
 
   
-  //sleep(1);
-// printf("after:\n");
   
-  printm(a,nlr,nlc,s);
+  //free memory and de-register variables
   
   matfreed(a);
   bsp_pop_reg(pa);
